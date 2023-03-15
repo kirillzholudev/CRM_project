856d55aa6fceb2c5ad09528bc3947e5eecbbecd1
kiryl <kirillzholudev24@gmail.com>
2023-03-15 03:41:11 +0100

    add dashboard simple page

--- CRM/urls.py
+++ CRM/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth import views
-from django.urls import path
+from django.urls import path, include
 
 from core.views import index
 from userprofile.views import sign
 
 urlpatterns = [
     path('', index, name='index'),
+    path('dashboard/', include('dashboard.urls')),
     path('sign-up/', sign, name='sign'),
     path('log-in/', views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
     path('log-out/', views.LogoutView.as_view(), name='logout'),
