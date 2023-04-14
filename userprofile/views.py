cd4c1a0bde2416432832be4d8120ec67a38a42a8
kiryl <kirillzholudev24@gmail.com>
2023-04-14 22:12:35 +0200

    add email sing in

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,30 +1,36 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
-
 from django.contrib import messages
 
 
 from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
 
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+
+
+
 def sign(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
 
         if form.is_valid():
-            if form.is_valid():
-                form.save()
-                username = form.cleaned_data.get('username')
-                password = form.cleaned_data.get('password1')
-                user = authenticate(request, username=username, password=password)
-                login(request, user)
+            user = form.save()
+            email = form.cleaned_data.get('email')
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(request, email=email, username=username, password=password)
 
+            if user is not None:
+                login(request, user)
                 return redirect('/dashboard/')
-            else:
-                messages.error(request, 'There was an error with your registration. Please try again.')
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"{field}: {error}")
+
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
 
     return render(request, 'userprofile/sign.html', {'form': form})
 
