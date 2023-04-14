4cc737234d7cabfc471e5d7bd558671c3deaf0a6
kiryl <kirillzholudev24@gmail.com>
2023-04-14 20:10:03 +0200

    fuc.ing changes

--- CRM/urls.py
+++ CRM/urls.py
@@ -33,7 +33,6 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    path('client/', include('client.urls')),
 ]
 
 
