621c84f2b08c05b7b3389a680444c45f0c3154b3
Dmitrii <chrisgrahm99@gmail.com>
2023-04-14 11:06:32 +0200

    django-allauth / urlpatterns / import, adds etc.

--- CRM/urls.py
+++ CRM/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('client/', include('client.urls')),
 ]
 
 
