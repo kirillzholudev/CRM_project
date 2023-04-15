02292c5f385897d6bdd35e9152e11df54d1d7ef6
Dmitrii <chrisgrahm99@gmail.com>
2023-04-15 16:04:23 +0200

    styled sign page / django widgets

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -6,11 +6,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
+
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, required=True, widget=forms.EmailInput())
-    username = forms.CharField(max_length=30, required=True, widget=forms.TextInput())
-    password1 = forms.CharField(max_length=254, required=True, label='Password', widget=forms.PasswordInput())
-    password2 = forms.CharField(max_length=254, required=True, label='Confirm Password', widget=forms.PasswordInput())
+    email = forms.EmailField(max_length=254, required=True, widget=forms.EmailInput(attrs={'class': 'form-field', 'autocorrect': 'off', 'autofocus': 'off', 'spellcheck': 'off', 'autocapitalize': 'off', 'value': '', 'autocomplete': 'username', 'inputmode': 'email'}))
+    username = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'class': 'form-field', 'autocorrect': 'off', 'autofocus': 'off', 'spellcheck': 'off', 'autocapitalize': 'off', 'value': '', 'autocomplete': 'username', 'inputmode': 'username'}))
+    password1 = forms.CharField(max_length=254, required=True, label='Password', widget=forms.PasswordInput(attrs={'class': 'form-field', 'autocomplete': 'password'}))
+    password2 = forms.CharField(max_length=254, required=True, label='Confirm Password', widget=forms.PasswordInput({'class': 'form-field', 'autocomplete': 'password'}))
 
     class Meta:
         model = User
