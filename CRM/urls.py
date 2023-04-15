e8ce0eb171a4b619c381dff1e7a8c007a1d30369
Dmitrii <chrisgrahm99@gmail.com>
2023-04-16 01:54:02 +0200

    update sign page

--- CRM/urls.py
+++ CRM/urls.py
@@ -15,12 +15,13 @@ urlpatterns = [
 
     path('', index, name='index'),
     path('dashboard/', include('dashboard.urls')),
-    path('dasboard/leads/', include('lead.urls')),
+    path('dashboard/leads/', include('lead.urls')),
     path('dashboard/clients/', include('client.urls')),
     path('accounts/sign-in/', sign, name='sign'),
     path('accounts/log-in/', views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
     path('log-out/', views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
+    path('clients/', include('client.urls')),
 
     path('accounts/', include('allauth.urls')),
 
